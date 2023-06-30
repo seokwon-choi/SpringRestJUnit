8e4cc3435df32ef9609284b96b05a1037d7c4518
최석원 <bluecircle2290@gmail.com>
2023-06-30 13:08:47 +0900

    controller get 메소드 test

--- src/test/java/com/example/SpringRestJUnit/controller/CalculatorApiControllerTest.java
+++ src/test/java/com/example/SpringRestJUnit/controller/CalculatorApiControllerTest.java
@@ -1,10 +1,48 @@
 package com.example.SpringRestJUnit.controller;
 
+import com.example.SpringRestJUnit.component.Calculator;
+import com.example.SpringRestJUnit.component.DollarCalculator;
+import com.example.SpringRestJUnit.component.MarketApi;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(CalculatorApiController.class)
 @AutoConfigureWebMvc
-
+@Import({Calculator.class, DollarCalculator.class})
 public class CalculatorApiControllerTest {
+    @MockBean
+    private MarketApi marketApi;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void init(){
+        Mockito.when(marketApi.connect()).thenReturn(3000);
+    }
+
+    @Test
+    public void sumTest() throws Exception {
+        //http://localhost:8080/api/sum
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("http://localhost:8080/api/sum")
+                        .queryParam("x", "10")
+                        .queryParam("y", "10")
+        ).andExpect(
+                MockMvcResultMatchers.status().isOk()
+        ).andExpect(
+                MockMvcResultMatchers.content().string("60000")
+        ).andDo(MockMvcResultHandlers.print());
+    }
 }
